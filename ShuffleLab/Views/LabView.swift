0743a71e8ac1f16906b89bc125be624b53ca14f6
Justin Cabral <ijustincabral@gmail.com>
2023-06-12 20:13:13 -0400

    Refined data model

---
+++ ShuffleLab/Views/LabView.swift
@@ -0,0 +1,18 @@
+//
+//  LabView.swift
+//  ShuffleLab
+//
+//  Created by Justin Cabral on 6/12/23.
+//
+
+import SwiftUI
+
+struct LabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabView()
+}
